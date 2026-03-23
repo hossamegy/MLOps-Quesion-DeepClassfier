9d78cdf47d882f59368977c5048021c612138773
Hossam <hossam.eldinegy@gmail.com>
2026-03-23 03:44:12 +0200

    update preprocessing pipeline

--- src/pipelines/preprocessing_pipeline.py
+++ src/pipelines/preprocessing_pipeline.py
@@ -1,4 +1,3 @@
-import os
 import sys
 from pathlib import Path
 
@@ -6,6 +5,7 @@ root_dir = Path(__file__).resolve().parents[2]
 if str(root_dir) not in sys.path:
     sys.path.insert(0, str(root_dir))
 
+import os
 import nltk
 from nltk.corpus import stopwords
 import mlflow
